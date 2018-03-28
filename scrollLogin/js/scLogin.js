74c22d9d1fe0bc3ea84b53631ed1bdb30ef1f1ee
Ing_Moises_M_G <moisesmg186@gmail.com>
2018-03-27 22:31:00 -0600

    Se creó otro inicio de sesión con una animación tipo scroll

---
+++ scrollLogin/js/scLogin.js
@@ -0,0 +1,37 @@
+/**
+ * @description Script que contiene las animaciones del 
+ *              formulario contenido en index.html
+ * 
+ * @author Ing. Moises M.G.
+ * @version 1.0.0
+ * @since 27/03/2018
+ * @see index.html, scLogin.css
+ */
+$(document).ready(function () {
+
+    /** @description Oculta el formulario de inicio de sesión
+     *               y muestra el de registro*/
+    $("#signup_btn").click(function () {
+        $("#main").animate({ left: "22.5%" }, 400);
+        $("#main").animate({ left: "30%" }, 500);
+        $("#loginform").css("visibility", "hidden");
+        $("#loginform").animate({ left: "25%" }, 400);
+
+        $("#signupform").animate({ left: "17%" }, 400);
+        $("#signupform").animate({ left: "30%" }, 500);
+        $("#signupform").css("visibility", "visible");
+    });
+
+    /** @description Oculta el formulario de registro
+     *               y muestra el de inicio de sesión*/
+    $("#login_btn").click(function () {
+        $("#main").animate({ left: "77.5%" }, 400);
+        $("#main").animate({ left: "70%" }, 500);
+        $("#signupform").css("visibility", "hidden");
+        $("#signupform").animate({ left: "75%" }, 400);
+
+        $("#loginform").animate({ left: "83.5%" }, 400);
+        $("#loginform").animate({ left: "70%" }, 500);
+        $("#loginform").css("visibility", "visible");
+    });
+});
